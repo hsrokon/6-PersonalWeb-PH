fe6fb8b04d91d3037d8911b3efd0b22586e9012b
MD. HUSSAIN SHAHRIAR ROKON <hsrokonofficial@gmail.com>
2024-08-22 23:30:23 +0600

    first commit-Just basic set ups-Nothing here

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'color1' : '',
+        'color2': ''
+      },
+      fontFamily: {
+        'font1' : ['Bebas Neue', 'sans-serif'],
+        'font2' : ['Roboto', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
